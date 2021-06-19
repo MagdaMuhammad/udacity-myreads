bf69d1d8d348c32ee9e7f62f3e5fb0ccc0ecaf92
Magda Muhammad <sonaz2012@gmail.com>
2021-06-19 20:18:44 +0400

    add search view

--- src/App.js
+++ src/App.js
@@ -2,8 +2,19 @@ import React, {Component} from 'react';
 import './App.css';
 import { Container, Row, Col } from 'reactstrap';
 import Section from './Section.js';
+import Search from './Search.js';
 
 class App extends Component {
+
+    state = {
+        showSearchPage: false;
+    }
+
+    showSearchPage = () => {
+        this.setState(()=>({
+            showSearchPage: true
+        }))
+    }
 
     render() {
         return (
@@ -15,13 +26,18 @@ class App extends Component {
                     <Container>
                         <Row>
                             <Col>
-                                <Section sectionName="Currently Reading"/>
-                                <Section sectionName="Want To Read"/>
-                                <Section sectionName="Read"/>
+                                { this.state.showSearchPage ?
+                                    <Search />
+                                :
+                                    <Section sectionName="Currently Reading"/>
+                                    <Section sectionName="Want To Read"/>
+                                    <Section sectionName="Read"/>
+                                }
                             </Col>
                         </Row>
                     </Container>
                 </div>
+                <button id="addBookBtn" title="Add New Book" onClick={this.showSearchPage}>Add New Book</button>
             </div>
         );
     }
---
+++ src/Search.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Section from './Section.js';
+
+
+class Search extends Component {
+
+
+	render() {
+		return (
+            <div className="sec-container">
+
+            	<Search sectionName="Search Results"/>
+            </div>
+		)
+	}
+}
+
+export default Search;
